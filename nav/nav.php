362ddbd0ceefd172064224d7e871bff29fcc06ba
The_WildThomas <thb5309@gmail.com>
2023-01-18 19:54:05 +0100

    correct nav bugs

--- nav/nav.php
+++ nav/nav.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" href="https://truzzcreations.netlify.app/nav/nav.css" />
 <nav id="nav-bar">
-    <div class="nav-block" id="block1" onclick="window.location.href='https://truzzcreations.glitch.me'">
+    <div class="nav-block" id="block1" onclick="window.location.href='https://truzzcreations.netlify.app'">
         <h3>Accueil</h3>
     </div>
-    <div class="nav-block" id="block2" onclick="window.location.href='https://truzzcreations.glitch.me/projects'">
+    <div class="nav-block" id="block2" onclick="window.location.href='https://truzzcreations.netlify.app/projects'">
         <h3>Mes projets</h3>
     </div>
-    <div class="nav-block" id="block3" onclick="window.location.href='https://truzzcreations.glitch.me/commander'">
+    <div class="nav-block" id="block3" onclick="window.location.href='https://truzzcreations.netlify.app/commander'">
         <h3>Commander</h3>
     </div>
 </nav>
